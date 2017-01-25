15088246c24c136a2d1e5b69e2908ead620c4dfb
Ketan Anjaria <fire@kidbombay.com>
2017-01-25 06:52:36 -0800

    Added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/about",   to: "pages#about",   as: :about
+  get "/contact", to: "pages#contact", as: :contact
+
+  root to: "pages#index"
 end
