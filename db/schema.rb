bfcfc73de691e4af7965605c1aef5c63ff45d895
Ketan Anjaria <fire@kidbombay.com>
2017-02-05 08:26:27 -0800

    Added acts as tree and parent id to role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205081936) do
+ActiveRecord::Schema.define(version: 20170205160947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20170205081936) do
     t.integer  "users_count", default: 0, null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "parent_id"
     t.index ["name"], name: "index_roles_on_name", unique: true, using: :btree
     t.index ["slug"], name: "index_roles_on_slug", unique: true, using: :btree
   end
