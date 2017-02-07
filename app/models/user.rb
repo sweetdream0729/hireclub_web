d6c886bcae3e18c9b20a1c5dda2593e701b8e271
Ketan Anjaria <fire@kidbombay.com>
2017-02-07 08:06:18 -0800

    Add counter culture for user locations

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ApplicationRecord
   has_many :user_roles, -> { order(position: :asc) }, dependent: :destroy, inverse_of: :user
   has_many :roles, through: :user_roles
   belongs_to :location
+  counter_culture :location, column_name: :users_count
 
 
   # Validations
