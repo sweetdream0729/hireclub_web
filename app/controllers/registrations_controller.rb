94bc3caa8994f4e19d122e68175d78666280a6c7
Ketan Anjaria <fire@kidbombay.com>
2017-02-11 14:40:22 -0800

    Add linkedin url to user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:username, :name, :email, :password, :avatar, :retained_avatar, :remove_avatar, 
       :location_id, :bio, 
-      :website_url, :twitter_url, :dribbble_url, :github_url, :medium_url, :facebook_url)
+      :website_url, :twitter_url, :dribbble_url, :github_url, :medium_url, :facebook_url, :linkedin_url)
   end
 
   protected
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211223549) do
+ActiveRecord::Schema.define(version: 20170211223754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,6 +181,7 @@ ActiveRecord::Schema.define(version: 20170211223549) do
     t.string   "github_url"
     t.string   "medium_url"
     t.string   "facebook_url"
+    t.string   "linkedin_url"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["gender"], name: "index_users_on_gender", using: :btree
