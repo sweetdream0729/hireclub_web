5597e7215b453c1f3f4d96a1f4560df444bc03da
Ketan Anjaria <fire@kidbombay.com>
2017-02-11 14:17:25 -0800

    fixed twitter display

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:username, :name, :email, :password, :avatar, :retained_avatar, :remove_avatar, :location_id, :bio, :website_url)
+    params.require(:user).permit(:username, :name, :email, :password, :avatar, :retained_avatar, :remove_avatar, :location_id, :bio, :website_url, :twitter_url)
   end
 
   protected
