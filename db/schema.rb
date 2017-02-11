6a52ebe2f424d6e815e43c69130d94f555ed0c93
Ketan Anjaria <fire@kidbombay.com>
2017-02-11 14:35:05 -0800

    Added medium url to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211222954) do
+ActiveRecord::Schema.define(version: 20170211223322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,6 +179,7 @@ ActiveRecord::Schema.define(version: 20170211222954) do
     t.string   "twitter_url"
     t.string   "dribbble_url"
     t.string   "github_url"
+    t.string   "medium_url"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["gender"], name: "index_users_on_gender", using: :btree
