4027e6e088a31efd6c388353f843d1d1da2f0c97
Ketan Anjaria <fire@kidbombay.com>
2017-03-12 09:15:09 -0700

    Update letsencrypt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,6 @@ class PagesController < ApplicationController
   end
 
   def letsencrypt
-    render text: "7VoffEQhb7932aBboNep2alVbFtTbgj9koohpnupb60.R7J5HZzwF8y8D6YdxAG8krBSycV1w9M5sE1GG7lC-oM"
+    render text: "N8qdmP5J9PeacjC9I-FRDfO7XabPSXS0xG9cPp6fICc.R7J5HZzwF8y8D6YdxAG8krBSycV1w9M5sE1GG7lC-oM"
   end
 end
