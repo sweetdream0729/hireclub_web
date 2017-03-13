fd313cc90a8ea7beb11fc47eb62007028a43a5b9
Blake Wills <blake.wills525@gmail.com>
2017-03-13 15:47:47 -0700

    #75 Completion steps 49 Edits - Userhelper, user_helper_spec, revised user_completion_spec
    Fix #49 Pull request

--- app/models/user_completion.rb
+++ app/models/user_completion.rb
@@ -1,59 +1,81 @@
+
 class UserCompletion
   attr_accessor :user, :link, :percent
+  
+  USERNAME_STEP = "Set Username"
+  LOCATION_STEP = "Set Location"
+  BIO_STEP = "Add Bio"
+  AVATAR_STEP = "Upload Profile Image"
+  ROLES_STEP = "Add Roles"
+  SKILLS_STEP = "Add Skills"
+  PROJECTS_STEP = "Add Projects"
+  MILESTONES_STEP = "Add Milestones"
+  WEBSITE_STEP = "Add Web Links"
 
   def initialize(user)
     @user = user
-    @percent = self.percent_complete
-    @link = self.next_link
   end
 
   def percent_complete
-    #started with 10% until last user_completion task decided
-    @percent = 10
+    percent = 0
 
-    @percent += 10 if user.username.present?
+    percent += 10 if user.username.present?
 
-    @percent += 10 if user.location.present?
+    percent += 10 if user.location.present?
 
-    @percent += 10 if user.bio.present?
+    percent += 10 if user.bio.present?
 
-    @percent += 10 if user.avatar_stored?
+    percent += 10 if user.avatar_stored?
 
-    @percent += 10 if user.roles.any?
+    percent += 10 if roles_complete?
 
-    @percent += 10 if user.skills.count > 4
+    percent += 10 if skills_complete?
 
-    @percent += 10 if user.projects.count > 2
+    percent += 10 if projects_complete?
 
-    @percent += 10 if user.milestones.count > 4
+    percent += 10 if milestones_complete?
 
-    @percent += 10 if user.website_url.present? || user.linkedin_url.present? || user.twitter_url.present? || user.dribbble_url.present? || user.facebook_url.present? || user.github_url.present? || user.medium_url.present?
+    percent += 10 if has_one_url?
 
-    @percent
+    percent
+
+  end
+
+
+  def roles_complete?
+    user.roles.any?
+  end
+
+  def skills_complete?
+    user.skills.count >= 5
+  end
+
+  def milestones_complete?
+    user.milestones.count >= 5
+  end
+
+  def projects_complete?
+    user.projects.count >= 3
+  end
+
+  def has_one_url?
+    user.website_url.present? || user.linkedin_url.present? || user.twitter_url.present? || user.dribbble_url.present? || user.facebook_url.present? || user.github_url.present? || user.medium_url.present?
   end
 
   def next_step
-    return "Set Username" if user.username.blank?
-    return "Set Location" if user.location.blank?
-    return "Complete Bio" if user.bio.blank?
-    return "Add avatar" if user.avatar.blank?
-    return "Add roles" if user.roles.blank?
-    return "Add 5 or more skills" if user.skills.count <= 4
-    return "Add 3 or more projects" if user.projects.count <= 2
-    return "Add 5 or more milestones" if user.milestones.count <= 4
-    return "Add website url" if user.website_url.blank?
-    return "Complete Profile"
-  end
 
-  def next_link
-    @link = "/settings" if self.next_step == "Set Username" || self.next_step == "Set Location" || self.next_step == "Complete Bio" || self.next_step == "Add avatar" || self.next_step == "Add website url"
-    @link = "/user_roles/new" if self.next_step == "Add roles"
-    @link = "/user_skills/new" if self.next_step == "Add 5 or more skills"
-    @link = "/#{@user.id}/projects/new" if self.next_step == "Add 3 or more projects"
-    @link = "/milestones/new" if self.next_step == "Add 5 or more milestones"
-    @link = "#" if @percent == 100
-    @link
+    return USERNAME_STEP if user.username.blank?
+    return LOCATION_STEP if user.location.blank?
+    return BIO_STEP if user.bio.blank?
+    return AVATAR_STEP if user.avatar.blank?
+    return ROLES_STEP if !roles_complete?
+    return SKILLS_STEP if !skills_complete?
+    return PROJECTS_STEP if !projects_complete?
+    return MILESTONES_STEP if !milestones_complete?
+    return WEBSITE_STEP if !has_one_url?
+    
   end
+  
 
 
 end
--- spec/models/user_completion_spec.rb
+++ spec/models/user_completion_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe UserCompletion, :type => :model do
   let(:user) { FactoryGirl.build(:user) }
   let(:user_completion) { UserCompletion.new(user) }
-  let(:randomlocation) { FactoryGirl.create(:location)}
+  let(:location) { FactoryGirl.create(:location)}
+  let(:username) { "testy" }
   subject { user_completion }
 
   describe "associations" do
@@ -14,110 +15,110 @@ RSpec.describe UserCompletion, :type => :model do
 
   context "percent_complete" do
     it "should return 10 for new user" do
+      expect(user_completion.percent_complete).to eq 0
+    end
+
+    it "should return 10 when username set" do
+      user.username = username
       expect(user_completion.percent_complete).to eq 10
     end
 
-    it "should return 20 when username set" do
-      user.username = "test"
-      expect(user_completion.percent_complete).to eq 20
-    end
-
-    it "should return 20 when website_url set" do
+    it "should return 10 when website_url set" do
       user.website_url = "http://hireclub.co"
-      expect(user_completion.percent_complete).to eq 20
+      expect(user_completion.percent_complete).to eq 10
     end
 
-    it "should return 20 when bio set" do
-      user.bio = "test"
-      expect(user_completion.percent_complete).to eq 20
+    it "should return 10 when bio set" do
+      user.bio = username
+      expect(user_completion.percent_complete).to eq 10
     end
 
-    it "should return 20 when location set" do
-      user.location = randomlocation
-      expect(user_completion.percent_complete).to eq 20
+    it "should return 10 when location set" do
+      user.location = location
+      expect(user_completion.percent_complete).to eq 10
     end
 
-    it "should return 20 when user role set" do
+    it "should return 10 when user role set" do
       user_role = FactoryGirl.create(:user_role, user: user)
-      expect(user_completion.percent_complete).to eq 20
+      expect(user_completion.percent_complete).to eq 10
     end
 
-    it "should return 20 when 5 skills added" do
+    it "should return 10 when 5 skills added" do
       5.times do
         FactoryGirl.create(:user_skill, user: user)
       end
 
-      expect(user_completion.percent_complete).to eq 20
+      expect(user_completion.percent_complete).to eq 10
     end
 
-    it "should return 20 when 3 projects added" do
+    it "should return 10 when 3 projects added" do
       3.times do
         FactoryGirl.create(:project, user: user)
       end
 
-      expect(user_completion.percent_complete).to eq 20
+      expect(user_completion.percent_complete).to eq 10
     end
 
-    it "should return 20 when 5 milestones added" do
+    it "should return 10 when 5 milestones added" do
       5.times do
         FactoryGirl.create(:milestone, user: user)
       end
 
-      expect(user_completion.percent_complete).to eq 20
+      expect(user_completion.percent_complete).to eq 10
     end
 
-    it "should return 20 when avatar set" do
+    it "should return 10 when avatar set" do
       user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
       user.save
-      expect(user_completion.percent_complete).to eq 20
+      expect(user_completion.percent_complete).to eq 10
     end
   end
 
   context "next_step" do
     it "should return Set Username when no username" do
-      expect(user_completion.next_step).to eq "Set Username"
+      expect(user_completion.next_step).to eq UserCompletion::USERNAME_STEP
     end
 
     it "should return Set Location when no location" do
-      user.username = "test"
-      expect(user_completion.next_step).to eq "Set Location"
+      user.username = username
+      expect(user_completion.next_step).to eq UserCompletion::LOCATION_STEP
     end
 
     it "should return Complete Bio when no bio" do
-      user.username = "test"
-      user.location = randomlocation
-      expect(user_completion.next_step).to eq "Complete Bio"
+      user.username = username
+      user.location = location
+      expect(user_completion.next_step).to eq UserCompletion::BIO_STEP
     end
 
     it "should return Add avatar when no avatar" do
-      user.username = "test"
-      user.location = randomlocation
+      user.username = username
+      user.location = location
       user.bio = "this is a bio"
-      expect(user_completion.next_step).to eq "Add avatar"
+      expect(user_completion.next_step).to eq UserCompletion::AVATAR_STEP
     end
 
     it "should return Add roles when no roles" do
-      user.username = "test"
-      user.location = randomlocation
+      user.username = username
+      user.location = location
       user.bio = "this is a bio"
       user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
       user.save
-      expect(user_completion.next_step).to eq "Add roles"
+      expect(user_completion.next_step).to eq UserCompletion::ROLES_STEP
     end
 
     it "should return Add 5 or more skills when less than 5 skills" do
-      user.username = "test"
-      user.location = randomlocation
+      user.username = username
+      user.location = location
       user.bio = "this is a bio"
       user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
       user_role = FactoryGirl.create(:user_role, user: user)
       user.save
-      expect(user_completion.next_step).to eq "Add 5 or more skills"
+      expect(user_completion.next_step).to eq UserCompletion::SKILLS_STEP
     end
 
     it "should return Add 2 or more projects when less than 2 projects" do
-      user.username = "test"
-      user.location = randomlocation
+      user.username = username
+      user.location = location
       user.bio = "this is a bio"
       user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
       user_role = FactoryGirl.create(:user_role, user: user)
@@ -125,12 +126,12 @@ RSpec.describe UserCompletion, :type => :model do
       5.times do
         FactoryGirl.create(:user_skill, user: user)
       end
-      expect(user_completion.next_step).to eq "Add 3 or more projects"
+      expect(user_completion.next_step).to eq UserCompletion::PROJECTS_STEP
     end
 
     it "should return Add 5 or more milestones when less than 5 milestones" do
-      user.username = "test"
-      user.location = randomlocation
+      user.username = username
+      user.location = location
       user.bio = "this is a bio"
       user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
       user_role = FactoryGirl.create(:user_role, user: user)
@@ -141,12 +142,12 @@ RSpec.describe UserCompletion, :type => :model do
       3.times do
         FactoryGirl.create(:project, user: user)
       end
-      expect(user_completion.next_step).to eq "Add 5 or more milestones"
+      expect(user_completion.next_step).to eq UserCompletion::MILESTONES_STEP
     end
 
     it "should return Add website_url when no website_url" do
-      user.username = "test"
-      user.location = randomlocation
+      user.username = username
+      user.location = location
       user.bio = "this is a bio"
       user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
       user_role = FactoryGirl.create(:user_role, user: user)
@@ -161,145 +162,12 @@ RSpec.describe UserCompletion, :type => :model do
         FactoryGirl.create(:milestone, user: user)
       end
 
-      expect(user_completion.next_step).to eq "Add website url"
-    end
-
-    it "should return as complete profile when profile complete" do
-      user.username = "test"
-      user.location = randomlocation
-      user.bio = "this is a bio"
-      user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
-      user_role = FactoryGirl.create(:user_role, user: user)
-      user.save
-      5.times do
-        FactoryGirl.create(:user_skill, user: user)
-      end
-      3.times do
-        FactoryGirl.create(:project, user: user)
-      end
-      5.times do
-        FactoryGirl.create(:milestone, user: user)
-      end
-      user.website_url = "http://hireclub.co"
-      expect(user_completion.next_step).to eq "Complete Profile"
+      expect(user_completion.next_step).to eq UserCompletion::WEBSITE_STEP
     end
 
   end 
 
-  context "next_link" do 
-    it "should return /settings when no username" do
-      expect(user_completion.next_link).to eq "/settings"
-    end
-
-    it "should return /settings no location" do
-      user.username = "test"
-      expect(user_completion.next_link).to eq "/settings"
-    end
-
-    it "should return /settings when no bio" do
-      user.username = "test"
-      user.location = randomlocation
-      expect(user_completion.next_link).to eq "/settings"
-    end
-
-    it "should return /settings when no avatar" do
-      user.username = "test"
-      user.location = randomlocation
-      user.bio = "this is a bio"
-      expect(user_completion.next_link).to eq "/settings"
-    end
-
-    it "should return /user_roles/new when no roles" do
-      user.username = "test"
-      user.location = randomlocation
-      user.bio = "this is a bio"
-      user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
-      user.save
-      expect(user_completion.next_link).to eq "/user_roles/new"
-    end
-
-    it "should return /user_skills/new when less than 5 skills" do
-      user.username = "test"
-      user.location = randomlocation
-      user.bio = "this is a bio"
-      user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
-      user_role = FactoryGirl.create(:user_role, user: user)
-      user.save
-      expect(user_completion.next_link).to eq "/user_skills/new"
-    end
-
-    it "should return /user_id/projects/new when less than 3 projects" do
-      user.username = "test"
-      user.location = randomlocation
-      user.bio = "this is a bio"
-      user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
-      user_role = FactoryGirl.create(:user_role, user: user)
-      user.save
-      5.times do
-        FactoryGirl.create(:user_skill, user: user)
-      end
-      id = user.id
-      expect(user_completion.next_link).to eq "/#{id}/projects/new"
-    end
-
-    it "should return /milestones/new when less than 5 milestones" do
-      user.username = "test"
-      user.location = randomlocation
-      user.bio = "this is a bio"
-      user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
-      user_role = FactoryGirl.create(:user_role, user: user)
-      user.save
-      5.times do
-        FactoryGirl.create(:user_skill, user: user)
-      end
-      3.times do
-        FactoryGirl.create(:project, user: user)
-      end
-      expect(user_completion.next_link).to eq "/milestones/new"
-    end
-
-    it "should return Add /settings when no website_url" do
-      user.username = "test"
-      user.location = randomlocation
-      user.bio = "this is a bio"
-      user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
-      user_role = FactoryGirl.create(:user_role, user: user)
-      user.save
-      5.times do
-        FactoryGirl.create(:user_skill, user: user)
-      end
-      3.times do
-        FactoryGirl.create(:project, user: user)
-      end
-      5.times do
-        FactoryGirl.create(:milestone, user: user)
-      end
-
-      expect(user_completion.next_link).to eq "/settings"
-    end
-
-    it "should return # when profile complete" do
-      user.username = "test"
-      user.location = randomlocation
-      user.bio = "this is a bio"
-      user.avatar = File.new("#{Rails.root}/spec/support/fixtures/image.png")
-      user_role = FactoryGirl.create(:user_role, user: user)
-      user.save
-      5.times do
-        FactoryGirl.create(:user_skill, user: user)
-      end
-      3.times do
-        FactoryGirl.create(:project, user: user)
-      end
-      5.times do
-        FactoryGirl.create(:milestone, user: user)
-      end
-      user.website_url = "http://hireclub.co"
-      expect(user_completion.next_link).to eq "#"
-    end
-
-  end 
-end
+ end
 
 
 
