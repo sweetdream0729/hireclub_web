45b34562c7ab8122ea719a51f82bb60f2bc272f9
Ketan Anjaria <fire@kidbombay.com>
2017-03-13 06:31:43 -0700

    Added login spec

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -17,7 +17,7 @@ module Features
       visit new_user_session_path
       fill_in 'Email', with: email
       fill_in 'Password', with: password
-      click_button 'login_button'
+      click_button 'Login'
     end
   end
 end
