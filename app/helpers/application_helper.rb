d05daf893790dc9c1c73e48719c54b1a0645889f
Ketan Anjaria <fire@kidbombay.com>
2017-03-18 08:53:57 -0700

    Default Company Avatar #87

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,14 @@ module ApplicationHelper
     end
   end
 
+  def company_avatar(image, size, use_https = false)
+    if image
+      image.remote_url(width: size, height: size, crop: :fill, secure: use_https, gravity: :north, format: "jpg", quality: 80)
+    else
+      cl_image_path("default_company_avatar.png")
+    end
+  end
+
   def image_placeholder_url(width, height)
     return "//placehold.it/#{width}x#{height}"
   end
