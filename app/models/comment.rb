2609cd4df373c64bfe59dc13779b1234f14d7657
Ketan Anjaria <fire@kidbombay.com>
2017-04-27 11:55:41 -0700

    Show comments on milestone and project page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,10 @@ class Comment < ApplicationRecord
   auto_strip_attributes :text, :squish => true
   nilify_blanks
 
+  # Scopes
+  scope :by_recent, -> {order(created_at: :desc)}
+  scope :by_oldest, -> {order(created_at: :asc)}
+
   # Associations
   belongs_to :user
   belongs_to :commentable, polymorphic: true
