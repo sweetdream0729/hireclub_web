2372f6798b3acdd35a35e0ff3196640c652c698b
Ketan Anjaria <fire@kidbombay.com>
2017-05-03 04:28:48 -0700

    Allow search by user name for job

--- app/models/job.rb
+++ app/models/job.rb
@@ -12,7 +12,7 @@ class Job < ApplicationRecord
   tracked only: [:create], owner: Proc.new{ |controller, model| model.user }
 
   include PgSearch
-  multisearchable :against => [:name, :company_name, :location_name, :full_time_name, :part_time_name, :remote_name, :contract_name, :internship_name]
+  multisearchable :against => [:name, :user_name, :company_name, :location_name, :full_time_name, :part_time_name, :remote_name, :contract_name, :internship_name]
 
   # Scopes
   scope :recent,       -> { order(created_at: :desc) }
@@ -40,6 +40,10 @@ class Job < ApplicationRecord
     ]
   end
 
+  def user_name
+    user.display_name
+  end
+  
   def company_name
     company.try(:name)
   end
