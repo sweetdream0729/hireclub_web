85b648e4fc69f81c52633d7ab48aa74767fbc13b
Ketan Anjaria <fire@kidbombay.com>
2017-05-03 18:30:07 -0700

    Validate presence of projection.position

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe Project, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:position) }
     #it { should validate_uniqueness_of(:slug).case_insensitive }
 
     it "should be valid only with approved skills" do
