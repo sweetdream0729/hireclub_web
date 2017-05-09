44a52b12b59a31c0325f170cfeed10121b1c6eaa
Ketan Anjaria <fire@kidbombay.com>
2017-05-08 23:56:33 -0700

    Add conversation_user.unread_messages_count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508162658) do
+ActiveRecord::Schema.define(version: 20170509062049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,8 +103,9 @@ ActiveRecord::Schema.define(version: 20170508162658) do
   create_table "conversation_users", force: :cascade do |t|
     t.integer  "conversation_id"
     t.integer  "user_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.integer  "unread_messages_count", default: 0, null: false
     t.index ["conversation_id", "user_id"], name: "index_conversation_users_on_conversation_id_and_user_id", unique: true, using: :btree
     t.index ["conversation_id"], name: "index_conversation_users_on_conversation_id", using: :btree
     t.index ["user_id"], name: "index_conversation_users_on_user_id", using: :btree
