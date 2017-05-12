99eb40f007bbf7f1fdb98771528e8b6b61ee9752
Ketan Anjaria <fire@kidbombay.com>
2017-05-11 23:10:53 -0700

    Disable citext extension

--- db/migrate/20170201131336_add_citext_extension.rb
+++ db/migrate/20170201131336_add_citext_extension.rb
@@ -1,9 +1,9 @@
 class AddCitextExtension < ActiveRecord::Migration[5.0]
   def up
-    ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS citext;")
+    #ActiveRecord::Base.connection.execute("CREATE EXTENSION IF NOT EXISTS citext;")
   end
 
   def down
-    ActiveRecord::Base.connection.execute("DROP EXTENSION citext;")
+    #ActiveRecord::Base.connection.execute("DROP EXTENSION citext;")
   end
 end
