455bdc3503412dd923dc8a39919812c4619d0c44
Blake Wills <blake.wills525@gmail.com>
2017-05-16 17:07:24 -0700

    #198 Add follow button to company partial

--- app/helpers/follow_helper.rb
+++ app/helpers/follow_helper.rb
@@ -1,8 +1,12 @@
 module FollowHelper
   
   def link_to_follow_company(company, user, options = {})
-    options[:class] = "" if options[:class].blank?
-
+    if options[:class].blank?
+      options[:class] = "" 
+    else
+      options["data-class"] = options[:class]
+    end
+    
     if user && user.following?(company)
       url = unfollow_company_path(company)
 
@@ -17,6 +21,7 @@ module FollowHelper
       options[:method] = :get
       options[:remote] = true if user.present?
       options[:class] += " company_follow_btn_#{company.id} btn btn-primary"
+
 
       link_to("Follow", url, options)
     end
