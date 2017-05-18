ac413e6b4907c4410452bdfffdd51b25666b58ce
Ketan Anjaria <fire@kidbombay.com>
2017-05-18 08:43:25 -0700

    Mentioned email

--- app/activities/mention_create_activity.rb
+++ app/activities/mention_create_activity.rb
@@ -2,6 +2,10 @@ class MentionCreateActivity
   KEY = "mention.create"
 
   def self.get_recipients_for(activity)
-    [activity.trackable.user]
+    [activity.trackable.user] - [activity.trackable.sender]
+  end
+
+  def self.send_notification(notification)
+    NotificationMailer.delay.comment_mentioned(notification)
   end
 end
