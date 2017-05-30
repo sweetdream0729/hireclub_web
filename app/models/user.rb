16d1dbfdb91c61843ca209aa38974597091df59f
abhi110892 <abhigyan@rubyeffect.com>
2017-05-30 12:21:39 +0530

    unread_messages_count field for user updated and unread count on restricted for conversations show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -434,6 +434,12 @@ class User < ApplicationRecord
     return user
   end
 
+  # update count of unread_messages_count field for user
+  def update_total_unread_msg_count
+    self.unread_messages_count = total_unread_msg_count
+    self.save
+  end
+
  # returns count of total unread messages for a user
   def total_unread_msg_count
     conversation_users.sum(:unread_messages_count)
