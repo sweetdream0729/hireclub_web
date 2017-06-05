62015b06cd33b96357218ca4d2f0843302da15e6
Ketan Anjaria <fire@kidbombay.com>
2017-06-05 08:54:11 -0700

    Add notification mailer spec

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,5 +1,34 @@
 require "rails_helper"
 
 RSpec.describe NotificationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+ 
+  before do
+    allow(Notification).to receive(:delay).and_return(Notification)
+  end
+
+  describe "follow_user" do
+    let(:user2) { FactoryGirl.create(:user) }
+    let(:mail) do 
+      user2.follow(user)
+      
+      activity = Activity.where(key: UserFollowActivity::KEY).last
+      expect(activity).to be_present
+
+      CreateNotificationJob.perform_now(activity)
+
+      notification = Notification.where(activity: activity).last
+      expect(notification).to be_present
+
+      NotificationMailer.user_followed(notification)
+    end
+
+    it 'renders the email' do
+      expect(mail.subject).to eq("#{user2.display_name} followed you on HireClub")
+      expect(mail.to).to eq([user.email])
+
+      #puts mail.header['X-MSYS-API'].value
+    end
+
+  end
 end
