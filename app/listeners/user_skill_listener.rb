7e15cc32ed3a1f35b2b5590a0672b19a513ada0f
Ketan Anjaria <fire@kidbombay.com>
2017-06-08 00:29:18 -0700

    Switch to counter cache for users years experience

--- app/listeners/user_skill_listener.rb
+++ app/listeners/user_skill_listener.rb
@@ -3,8 +3,6 @@ class UserSkillListener
     user = user_skill.user
     return unless user.present?
 
-    user.update_years_experience
-
     user.reload
     if user.user_skills.count >= 5
       Badge.reward_skill_badge(user)
