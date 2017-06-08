fb6424aaa290a21812e356def6a0341b408bb70c
Ketan Anjaria <fire@kidbombay.com>
2017-06-08 00:23:04 -0700

    first pass at placement model

---
+++ app/models/placement.rb
@@ -0,0 +1,26 @@
+class Placement < ApplicationRecord
+  # Extensions
+  acts_as_taggable_array_on :tags
+  include HasTagsList
+  has_tags_list :tags
+
+  # Scopes
+  scope :by_priority,    -> { order(priority: :asc) }
+  
+  # Associations
+  belongs_to :placeable, polymorphic: true
+
+  # Validations
+  validates :placeable, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate  :end_time_is_after_start_time
+
+
+  protected
+  def end_time_is_after_start_time
+    if self.start_time.present? && self.end_time.present? && self.end_time < self.start_time
+      errors.add(:end_time, "End Time must be after Start Time")
+    end
+  end
+end
---
+++ spec/models/placement_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe Placement, type: :model do
+  let(:placement) { FactoryGirl.build(:placement) }
+
+  subject { placement }
+
+  describe "associations" do
+    it { should belong_to(:placeable) }
+    
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:placeable) }
+    it { should validate_presence_of(:start_time) }
+    it { should validate_presence_of(:end_time) }
+
+    it "should validate that end time is after start_time" do
+    	placement.end_time = placement.start_time - 1.minute
+    	expect(placement).to be_invalid
+
+    	placement.end_time = placement.start_time + 1.minute
+    	expect(placement).to be_valid
+    end
+  end
+
+
+  describe 'tags' do 
+    it "should let you add tags" do
+      placement.tags_list = "foo, bar"
+      placement.save
+
+      expect(placement).to be_valid
+      expect(placement.tags[0]).to eq "foo"
+      expect(placement.tags[1]).to eq "bar"      
+    end 
+  end
+
+  describe "scopes" do
+  	it "should sort placements by lowest priority first" do
+  		placement1 = FactoryGirl.create(:placement, priority: 1)
+			placement2 = FactoryGirl.create(:placement, priority: 2)
+			placement3 = FactoryGirl.create(:placement, priority: 3)
+
+			placements = Placement.by_priority
+
+			expect(placements[0]).to eq placement1
+			expect(placements[1]).to eq placement2
+			expect(placements[2]).to eq placement3
+  	end
+  end
+
+end
