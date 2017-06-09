0a1d21283d27761d46020a24a8898946916b84b3
Ketan Anjaria <fire@kidbombay.com>
2017-06-09 01:26:38 -0700

    For protocol to be http for action mailer on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "hireclub_web_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { 
+    :host => Rails.application.secrets.domain_name
+    :protocol => "http"
+  }
 
   # ActionMailer::Base.smtp_settings = {
   #   :user_name => Rails.application.secrets.sendgrid_username,
