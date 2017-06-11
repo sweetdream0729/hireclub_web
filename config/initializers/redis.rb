f991a5061a4aeab048af439a73970c9b9d1bc317
Ketan Anjaria <fire@kidbombay.com>
2017-06-11 09:12:12 -0700

    Use redis_url as config key for redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-# if Rails.application.secrets.redis_provider.present?
-#   $redis = Redis.new(:url => Rails.application.secrets.redis_provider)
-# end
+if Rails.application.secrets.redis_url.present?
+  $redis = Redis.new(:url => Rails.application.secrets.redis_url)
+end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.secrets.redis_provider }
+  config.redis = { url: Rails.application.secrets.redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.secrets.redis_provider }
+  config.redis = { url: Rails.application.secrets.redis_url }
 end
