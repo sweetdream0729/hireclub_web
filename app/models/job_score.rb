976913721d7d19a3761d7b1487237fae9a407d1a
Blake Wills <blake.wills525@gmail.com>
2017-06-13 16:33:38 -0700

    #165 Add roles to calculate job score

--- app/models/job_score.rb
+++ app/models/job_score.rb
@@ -8,22 +8,47 @@ class JobScore < ApplicationRecord
   validates :user_id, uniqueness: { scope: :job_id }
   validates :score, numericality: { greater_than_or_equal_to: 0 }
 
+  def role_match?
+    self.user.user_roles.each do |role|
+      return true if role.name === self.job.name 
+    end
+    false
+  end
+
   def update_score
     return if user.nil? || job.nil?
     new_score = 0
+
+    new_score = calc_skills(new_score)
+    new_score = calc_roles(new_score)
+    
+    self.score = [new_score,0].max
+    self.save
+  end
+
+  def calc_skills(score)
+    # Add user_skill.years to score
+    # minimum score of 1 per matching skill
+    
     job.skills.each do |skill_name|
-      
       skill = Skill.where(name: skill_name)
       next if skill.nil?
 
       user_skill = user.user_skills.where(skill: skill).first
       if user_skill
         increment = [user_skill.years,1].max
-        new_score += increment
+        score += increment
       end
     end
-    
-    self.score = [new_score,0].max
-    self.save
+    return score
+  end
+
+  def calc_roles(score)
+    # Add 5 if any role matches
+    if user.roles.include?(job.role)
+      score += 5
+    end
+
+    return score
   end
 end
