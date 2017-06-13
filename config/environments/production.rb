f0fe29cdc14beedfa0b64c62f3af1a4d05c68ac8
Ketan Anjaria <fire@kidbombay.com>
2017-06-13 06:33:32 -0700

    Remove send-grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,16 +63,6 @@ Rails.application.configure do
     :protocol => "http"
   }
 
-  # ActionMailer::Base.smtp_settings = {
-  #   :user_name => Rails.application.secrets.sendgrid_username,
-  #   :password => Rails.application.secrets.sendgrid_password,
-  #   :domain => Rails.application.secrets.domain_name,
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
-
   ActionMailer::Base.smtp_settings = {
     domain: Rails.application.secrets.domain_name,
     user_name: 'SMTP_Injection',
