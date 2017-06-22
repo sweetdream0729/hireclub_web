cfee1c56a8d7b6d86f545f5af4bb26c68b2c0a65
Ketan Anjaria <fire@kidbombay.com>
2017-06-22 04:53:38 -0700

    use Cloudinary auto format and auto quality settings for images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   
   def model_image(image, width, height, use_https = false)
     if image
-      image.remote_url(width: width, height: height, crop: :fill, secure: use_https, gravity: :north, format: "jpg", quality: 80)
+      image.remote_url(width: width, height: height, crop: :fill, secure: use_https, gravity: :north, fetch_format: "auto", quality: "auto")
     else
       image_placeholder_url(width,height)
     end
@@ -13,7 +13,7 @@ module ApplicationHelper
 
   def model_avatar(image, size, use_https = false)
     if image
-      image.remote_url(width: size, height: size, crop: :fill, secure: use_https, gravity: :north, format: "jpg", quality: 80)
+      image.remote_url(width: size, height: size, crop: :fill, secure: use_https, gravity: :north, fetch_format: "auto", quality: "auto")
     else
       cl_image_path("default_avatar.png", {secure: use_https})
     end
@@ -21,7 +21,7 @@ module ApplicationHelper
 
   def company_avatar(image, size, use_https = false)
     if image
-      image.remote_url(width: size, height: size, crop: :fill, secure: use_https, gravity: :north, format: "jpg", quality: 80)
+      image.remote_url(width: size, height: size, crop: :fill, secure: use_https, gravity: :north, fetch_format: "auto", quality: "auto")
     else
       cl_image_path("default_company_avatar.png", {secure: use_https})
     end
