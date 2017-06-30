323a333a04f7f0197a45adf25132c3c0d5576139
Ketan Anjaria <fire@kidbombay.com>
2017-06-30 05:05:33 -0700

    Fix listener for analytics bounce keys

--- app/listeners/analytics_event_listener.rb
+++ app/listeners/analytics_event_listener.rb
@@ -4,16 +4,17 @@ class AnalyticsEventListener
   end
 
   def process_email_bounce(analytics_event)
-    return unless analytics_event.key == EmailBounceActivity::KEY || EmailBounceActivity::REJECTION_KEY
+    if analytics_event.key == EmailBounceActivity::KEY || analytics_event.key == EmailBounceActivity::REJECTION_KEY
 
-    data = analytics_event.data
-    campaign_id = data["campaign_id"]
-    rcpt_meta = data["rcpt_meta"]
-    
-    if data.present? && rcpt_meta.present? && campaign_id == InviteCreateActivity::KEY 
-      invite = Invite.where(id: rcpt_meta["invite_id"].to_i).first
-      if invite.present?
-        invite.mark_bounced!
+      data = analytics_event.data
+      campaign_id = data["campaign_id"]
+      rcpt_meta = data["rcpt_meta"]
+      
+      if data.present? && rcpt_meta.present? && campaign_id == InviteCreateActivity::KEY 
+        invite = Invite.where(id: rcpt_meta["invite_id"].to_i).first
+        if invite.present?
+          invite.mark_bounced!
+        end
       end
     end
   end
