9cdd81d2d21096bbf044303912365a940011089d
Ketan Anjaria <fire@kidbombay.com>
2017-06-30 05:27:17 -0700

    Track message events

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,10 +2,12 @@ class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def sparkpost
-    Rails.logger.info params.inspect
+    #Rails.logger.info params.inspect
 
     params[:_json].each do |item|
-      Rails.logger.info item.inspect      
+      if item["msys"] && item["msys"]["message_event"]
+        Rails.logger.info item.inspect 
+      end
     end
   end
 end
