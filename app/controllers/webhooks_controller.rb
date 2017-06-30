77814f959d7a7880013c410e632aad4521b5bfd6
Ketan Anjaria <fire@kidbombay.com>
2017-06-30 05:30:16 -0700

    Create message events from web hook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -6,7 +6,8 @@ class WebhooksController < ApplicationController
 
     params[:_json].each do |item|
       if item["msys"] && item["msys"]["message_event"]
-        Rails.logger.info item.inspect 
+        Rails.logger.info item.inspect
+        SparkpostService.create_from_sparkpost_message_event(item["msys"]["message_event"])
       end
     end
   end
