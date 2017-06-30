c737884821852b8efabea3f4d4bc981c9235422a
Ketan Anjaria <fire@kidbombay.com>
2017-06-30 05:20:26 -0700

    Log items

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,7 +1,11 @@
 class WebhooksController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def sparkpost
     Rails.logger.info params.inspect
+
+    params[:_json].each do |item|
+      Rails.logger.info item.inspect      
+    end
   end
 end
