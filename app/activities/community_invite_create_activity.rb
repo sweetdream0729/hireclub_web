9454870480d574a485903257356079fd1c7e6a1a
Ketan Anjaria <fire@kidbombay.com>
2017-07-12 07:02:15 -0700

    Add simple email for community invite

--- app/activities/community_invite_create_activity.rb
+++ app/activities/community_invite_create_activity.rb
@@ -4,4 +4,8 @@ class CommunityInviteCreateActivity
   def self.get_recipients_for(activity)
     [activity.recipient]
   end
+
+  def self.send_notification(notification)
+    NotificationMailer.community_invited(notification).deliver_later
+  end
 end
