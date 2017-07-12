2518c381d8332f3d59d9a9c5fd64fece3affda72
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2017-07-12 10:45:37 -0700

    Fix migration exception by adding enable extension citext

--- db/migrate/20170201131431_add_user_name_to_user.rb
+++ db/migrate/20170201131431_add_user_name_to_user.rb
@@ -1,5 +1,6 @@
 class AddUserNameToUser < ActiveRecord::Migration[5.0]
   def change
+    enable_extension 'citext'
     add_column :users, :username, :citext
     add_index  :users, :username, unique: true
   end
