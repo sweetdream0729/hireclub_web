09d8668423676fb5f5c15e03d37f68689977be01
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2017-07-12 11:47:48 -0700

    add grace as an admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,7 @@ Skill.seed
 Role.seed
 user = UserService.create_admin_user('fire@kidbombay.com', 'testtest')
 user = UserService.create_admin_user('blakewills525@yahoo.com', 'testtest')
+user = UserService.create_admin_user('stardusts_25@yahoo.com', 'testtest')
+
 Company.import
 Badge.seed
