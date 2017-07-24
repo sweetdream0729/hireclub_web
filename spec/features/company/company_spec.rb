90f85f2af4a40ef02ec0db922c1d930eaba6a9c3
abhi110892 <abhigyan@rubyeffect.com>
2017-07-24 17:47:38 +0530

    comment corrected in company spec

--- spec/features/company/company_spec.rb
+++ spec/features/company/company_spec.rb
@@ -15,12 +15,12 @@ RSpec.feature "Company", :type => :feature do
   scenario "User should be able tovisit company show page " do
   	company1 = FactoryGirl.create(:company)
 
-  	#visiting job using job url
+  	#visiting company using company url
   	visit(company_path(company1.id))
   	expect(page).to have_content company1.name
   	expect(page).to have_content company1.tagline
 
-  	#visiting job by clicking on job link on jobs index
+  	#visiting company by clicking on company link on companies index
   	visit(companies_path)
   	click_link(company1.name)
   	expect(page).to have_content company1.name
