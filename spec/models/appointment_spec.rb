b4f46db6e7217b8ba86ffe43507a0411016e8609
shreek271 <shreenandan@rubyeffect.com>
2017-07-25 17:13:41 +0530

    uncommented appointment_spec code

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -19,49 +19,49 @@ RSpec.describe Appointment, type: :model do
     it { appointment.save; should validate_uniqueness_of(:acuity_id) }
   end
 
-  # describe 'cancel' do
-  #   it "should cancel!" do
-  #     appointment.cancel!
+  describe 'cancel' do
+    it "should cancel!" do
+      appointment.cancel!
 
-  #     expect(appointment.canceled_at).not_to be_nil
-  #     expect(appointment.canceled?).to eq true
-  #     expect(appointment.active?).to eq false
-  #     expect(appointment.status).to eq "Canceled"
-  #   end
-  # end
+      expect(appointment.canceled_at).not_to be_nil
+      expect(appointment.canceled?).to eq true
+      expect(appointment.active?).to eq false
+      expect(appointment.status).to eq "Canceled"
+    end
+  end
 
-  # describe "reschedule!" do
-  #   it "should reschedule!" do
-  #     new_start_time = DateTime.now + 1.day
-  #     new_end_time = new_start_time + 1.hour
-  #     appointment.reschedule!(new_start_time, new_end_time)
+  describe "reschedule!" do
+    it "should reschedule!" do
+      new_start_time = DateTime.now + 1.day
+      new_end_time = new_start_time + 1.hour
+      appointment.reschedule!(new_start_time, new_end_time)
 
-  #     appointment.reload
+      appointment.reload
 
-  #     expect(appointment.start_time).to eq new_start_time
-  #     expect(appointment.end_time).to eq new_end_time
-  #   end
-  # end
+      expect(appointment.start_time).to eq new_start_time
+      expect(appointment.end_time).to eq new_end_time
+    end
+  end
 
-  # describe "complete!" do
-  #   let(:completer) { FactoryGirl.build(:user) }
-  #   it "should complete!" do
-  #     appointment.complete!(completer)
+  describe "complete!" do
+    let(:completer) { FactoryGirl.build(:user) }
+    it "should complete!" do
+      appointment.complete!(completer)
 
-  #     appointment.reload
+      appointment.reload
 
-  #     expect(appointment.completed_on).not_to be_nil
-  #     expect(appointment.completed_by).to eq completer
+      expect(appointment.completed_on).not_to be_nil
+      expect(appointment.completed_by).to eq completer
 
-  #     activity = Activity.where(key: AppointmentCompleteActivity::KEY).last
-  #     expect(activity).to be_present
+      activity = Activity.where(key: AppointmentCompleteActivity::KEY).last
+      expect(activity).to be_present
 
-  #     expect(activity.owner).to eq completer
-  #     expect(activity.private).to eq true
-  #     expect(activity.recipient).to eq appointment.user
+      expect(activity.owner).to eq completer
+      expect(activity.private).to eq true
+      expect(activity.recipient).to eq appointment.user
 
-  #   end
-  # end
+    end
+  end
 
   describe "search" do
     it "should search in order user, category, assigned" do
