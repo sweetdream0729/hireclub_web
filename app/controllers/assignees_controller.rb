d74614a478810f995c18a969c05643048e0cd209
abhi110892 <abhigyan@rubyeffect.com>
2017-07-25 19:49:33 +0530

    removed code for helper add message

--- app/controllers/assignees_controller.rb
+++ app/controllers/assignees_controller.rb
@@ -2,13 +2,10 @@ class AssigneesController < ApplicationController
 
 	def create
 		@appointment = Appointment.find(params[:assignee][:appointment_id])
-		old_count = @appointment.assignees.count
 		params[:users].each do |user_id|
 			assignee = @appointment.assignees.where(user_id: user_id).first_or_create
 		end
 		@assignees = @appointment.assignees
 		
-		new_count = @assignees.count
-		@count = new_count - old_count
 	end
 end
