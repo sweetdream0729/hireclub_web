debf3e2b8bff42a0142d348e3f91688e71dfb435
abhi110892 <abhigyan@rubyeffect.com>
2017-07-25 19:26:28 +0530

    Added message after assignee is added

--- spec/models/assignee_spec.rb
+++ spec/models/assignee_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Assignee, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:appointment) }
   end
+
+  describe 'validations' do
+  	it { should validate_presence_of(:user) }
+    it { should validate_presence_of(:appointment) }
+  end
 end
