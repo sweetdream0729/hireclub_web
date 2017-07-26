eab96c55e7cd8ec2bd9f812f89a9c1489ffa07d3
abhi110892 <abhigyan@rubyeffect.com>
2017-07-26 20:47:53 +0530

    parameter format for assign me link changed

--- app/controllers/assignees_controller.rb
+++ app/controllers/assignees_controller.rb
@@ -2,22 +2,11 @@ class AssigneesController < ApplicationController
 	after_action :verify_authorized, only: [:create]
 
 	def create
-		@flag = 0
-
-		#when request is from the form
-		if params.has_key?(:assignee) && params.has_key?(:users)
-			@appointment = Appointment.find(params[:assignee][:appointment_id])
-			params[:users].each do |user_id|
-				assignee = @appointment.assignees.where(user_id: user_id).first_or_initialize
-				authorize(assignee)
-				assignee.save
-			end
-		else #when assign me link is clicked
-			@appointment = Appointment.find(params[:appointment_id])
-			assignee = @appointment.assignees.where(user_id: current_user.id).first_or_initialize
+		@appointment = Appointment.find(params[:assignee][:appointment_id])
+		params[:users].each do |user_id|
+			assignee = @appointment.assignees.where(user_id: user_id).first_or_initialize
 			authorize(assignee)
 			assignee.save
-			@flag = 1 #for removing assign me link
 		end
 		@assignees = @appointment.assignees
 		
