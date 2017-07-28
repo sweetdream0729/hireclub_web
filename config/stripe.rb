ef290be777a3f8d9729b92cfe2f841b3d96930a4
Ketan Anjaria <fire@kidbombay.com>
2017-07-28 10:19:54 -0700

    Turn on charge succeeded web hook

--- config/stripe.rb
+++ config/stripe.rb
@@ -1,5 +1,6 @@
 StripeEvent.configure do |events|
   events.subscribe 'customer.subscription.created', StripeAction::SubscriptionCreated.new
+  events.subscribe 'charge.succeeded', StripeAction::ChargeSucceeded.new
 end
 
 Stripe.api_version = "2016-03-07"
