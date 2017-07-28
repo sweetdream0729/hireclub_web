2301425612032e6193aba7c624b5ffa5208bd531
Ketan Anjaria <fire@kidbombay.com>
2017-07-28 10:30:31 -0700

    Added logging for stripe_event

--- app/actions/stripe_action/charge_succeeded.rb
+++ app/actions/stripe_action/charge_succeeded.rb
@@ -1,6 +1,7 @@
 class StripeAction::ChargeSucceeded < StripeAction::Base
 
   def call(stripe_event)
+    Rails.logger.info stripe_event
     stripe_charge = Stripe::Charge.retrieve({ id: stripe_event.data.object.id, expand: ['balance_transaction'] })
     payment = Payment.create_from_stripe_charge(stripe_charge)
     return payment
