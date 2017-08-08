ca55bd3b48c0624f2bc33c6fea090cd151c800b3
Ketan Anjaria <fire@kidbombay.com>
2017-08-08 05:45:08 -0700

    Add logging for stripe events

--- app/actions/stripe_action/card_created.rb
+++ app/actions/stripe_action/card_created.rb
@@ -1,6 +1,7 @@
 class StripeAction::CardCreated < StripeAction::Base
 
   def call(stripe_event)
+    Rails.logger.info "CardCreated #{stripe_event.inspect}"
     stripe_card = stripe_event.data.object
     user = User.find_by(stripe_customer_id: stripe_card.customer)
     if user.present?
