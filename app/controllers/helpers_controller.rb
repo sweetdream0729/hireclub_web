c4340b8b947149636af80ffd1c74371ab32eaaeb
Ketan Anjaria <fire@kidbombay.com>
2017-08-11 08:33:38 -0700

    Added how it works section

--- app/controllers/helpers_controller.rb
+++ app/controllers/helpers_controller.rb
@@ -1,6 +1,5 @@
 class HelpersController < ApplicationController
   def index
     @appointment_categories = AppointmentCategory.published.by_priority
-    @appointment_types = AppointmentType.by_priority
   end
 end
