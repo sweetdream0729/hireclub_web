75412129d04e452c37f42d815b8f211a981b89d1
abhi110892 <abhigyan@rubyeffect.com>
2017-08-30 20:01:15 +0530

    removed debug statement

--- app/activities/appointment_cancel_activity.rb
+++ app/activities/appointment_cancel_activity.rb
@@ -3,10 +3,7 @@ class AppointmentCancelActivity
 
   def self.get_recipients_for(activity)
     recipients = activity.trackable.assigned_users.to_ary
-    puts "\n\n\n #{recipients} \n\n\n"
     recipients << activity.trackable.user
-    puts "\n\n\n #{recipients} \n\n\n"
-    return recipients
   end
 
   def self.send_notification(notification)
