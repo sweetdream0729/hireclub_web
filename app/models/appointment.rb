a6f198b5d67fcc2e85471935fc577cb107a09c2a
Ketan Anjaria <fire@kidbombay.com>
2017-09-17 12:35:28 -0700

    #462

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -27,16 +27,17 @@ class Appointment < ApplicationRecord
   monetize :payout_price_cents
 
   # Scopes
-  scope :active,        -> { where(canceled_at: nil) }
-  scope :incomplete,    -> { where(completed_on: nil) }
-  scope :unassigned,    -> { where(assignees_count: 0) }
-  scope :paid,          -> { where(paid_out: true) }
-  scope :unpaid,        -> { where(paid_out: false) }
-  scope :canceled,      -> { where("appointments.canceled_at IS NOT NULL") }
-  scope :completed,     -> { where("appointments.completed_on IS NOT NULL") }
-  scope :upcoming,      -> { where('appointments.start_time > ?', Time.current) }
-  scope :by_start_time, -> { order('appointments.start_time', 'appointments.id') }
-  scope :by_recent,     -> { order('appointments.start_time desc', 'appointments.id') }
+  scope :active,           -> { where(canceled_at: nil) }
+  scope :incomplete,       -> { where(completed_on: nil) }
+  scope :unassigned,       -> { where(assignees_count: 0) }
+  scope :paid,             -> { where(paid_out: true) }
+  scope :unpaid,           -> { where(paid_out: false) }
+  scope :canceled,         -> { where("appointments.canceled_at IS NOT NULL") }
+  scope :completed,        -> { where("appointments.completed_on IS NOT NULL") }
+  scope :upcoming,         -> { where('appointments.start_time > ?', Time.current) }
+  scope :by_start_time,    -> { order('appointments.start_time', 'appointments.id') }
+  scope :by_recent,        -> { order('appointments.start_time desc', 'appointments.id') }
+  scope :created_between,  -> (start_date, end_date) { where("created_at BETWEEN ? and ?", start_date, end_date) }
 
   # Associations
   belongs_to :user
